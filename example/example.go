f43a641594d4769abcb6927364873681096d6d08
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-03-14 17:16:21 +0300

    Run gofmt on example.go

--- example/example.go
+++ example/example.go
@@ -21,10 +21,10 @@ func main() {
 	clients = append(clients, client{Name: "John Sebastine", Age: 33, Height: 159})
 	clients = append(clients, client{Name: "Markus Hallberg", Age: 18, Height: 169})
 	clients = append(clients, client{Name: "John Wonapaska", Age: 59, Height: 170})
-	
+
 	cw := csv.NewWriter(os.Stdout)
 	w := cswizard.New(cw)
-	
+
 	// TODO:
 	// 1. Add an empty column between Name and Age;
 	// 2. Swap Age and Height columns;
@@ -32,28 +32,28 @@ func main() {
 	//
 	// Try to play around with these tasks, and you will get a rough idea
 	// why CSWizard was worth creating.
-	
+
 	colName := w.AddHeader("Client name")
 	colAge := w.AddHeader("Client age")
 	colHeight := w.AddHeader("Client height (predicted)")
-	
+
 	err := w.LockHeaders()
 	if err != nil {
 		return
 	}
-	
+
 	for _, c := range clients {
 		row := w.CreateRow()
 		row[colName] = c.Name
 		row[colAge] = strconv.FormatUint(c.Age, 10)
 		row[colHeight] = strconv.FormatUint(c.Height, 10)
-		
+
 		err := w.CommitRow(row)
 		if err != nil {
 			return
 		}
 	}
-	
+
 	cw.Flush()
 
 	return
